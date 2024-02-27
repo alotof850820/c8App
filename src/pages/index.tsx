95ab3d56d13f673a124b07f2f970cf48425b1fc5
ken <ken@ax2tw.net>
2024-02-27 13:21:51 +0800

    add recommend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,11 @@ import TopBar from "@/components/TopBar";
 import Carousel from "@/components/Carousel";
 import Item from "@/components/Item";
 import Footer from "@/components/Footer";
+import Recommend from "@/components/Recommend";
+
 
 const Index = () => {
+
   return (
     <div className="index">
       <TopBar num={0}></TopBar>
@@ -12,6 +15,7 @@ const Index = () => {
       <Item></Item>
       <Item></Item>
       <Item></Item>
+      <Recommend></Recommend>
       <Footer></Footer>
     </div>
   );
