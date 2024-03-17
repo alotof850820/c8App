d282ff1fbfbd9877c59d82dc22ff2dc311b1383f
alotof850820 <119794885+alotof850820@users.noreply.github.com>
2024-03-17 13:23:47 +0800

    add dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     port: 5050,
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+  },
+});
